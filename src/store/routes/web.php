15b82eb3c9b7a189aaf0826b5602a3ef994346e7
bgc <feeled@gmail.com>
2022-06-26 01:43:52 +0900

    ルート修正

--- src/store/routes/web.php
+++ src/store/routes/web.php
@@ -14,5 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    //return view('welcome');
+    return redirect('http://localhost:8080/docs/');
 });
