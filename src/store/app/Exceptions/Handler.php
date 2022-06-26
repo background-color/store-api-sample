0c4cd45284c092083ff1357a9c767d72a66a6684
bgc <feeled@gmail.com>
2022-06-26 11:49:54 +0900

    例外エラーをjsonで返す

--- src/store/app/Exceptions/Handler.php
+++ src/store/app/Exceptions/Handler.php
@@ -3,10 +3,18 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Auth\AuthenticationException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Log;
+use App\Traits\ResponseTrait;
 use Throwable;
 
 class Handler extends ExceptionHandler
 {
+    use ResponseTrait;
+
     /**
      * A list of the exception types that are not reported.
      *
@@ -34,8 +42,25 @@ class Handler extends ExceptionHandler
      */
     public function register()
     {
-        $this->reportable(function (Throwable $e) {
-            //
+        $this->renderable(function (Throwable $e, Request $request) {
+            if ($request->is('api/*')) {
+                Log::error('API: '.$request->method().': '.$request->fullUrl());
+                Log::error('Class: ' . get_class($e));
+                Log::error('Message: ' . $e->getMessage());
+                
+                if ($this->isHttpException($e)) {
+                    $message = $e->getMessage() ?: Response::$statusTexts[$e->getStatusCode()];
+                    return $this->getErrorResponse($message, $e->getStatusCode());
+                }
+                if ($e instanceof AuthenticationException) {
+                    return $this->getErrorResponse($e->getMessage(), Response::HTTP_UNAUTHORIZED);
+                }
+                /*
+                if ($e instanceof ValidationException) {
+
+                }
+                */
+            }
         });
     }
 }
